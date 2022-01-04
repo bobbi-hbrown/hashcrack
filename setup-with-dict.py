8e6a6b6e4f6058e333d3a27a22afa7b21a47909e
bobbi-hbrown <rholdingbrown@gmail.com>
2022-01-04 17:02:19 +0000

    add http exception error handling and move some variables

--- setup-with-dict.py
+++ setup-with-dict.py
@@ -10,6 +10,7 @@
 
 import os
 import urllib.request
+import urllib.error
 import zipfile
 import shutil
 
@@ -21,6 +22,14 @@ def btexec( sexec ):
     os.system(sexec)
     
 def main():
+
+    # define dictionary URLs here
+    empdict = "http://www.blacktraffic.co.uk/pw-dict-public/empdict.zip"
+    JksPrivkPrepare = "https://github.com/floyd-fuh/JKS-private-key-cracker-hashcat/raw/master/JksPrivkPrepare.jar"
+    # change this to your preferred hashcat version
+    hashcat_latest = "https://hashcat.net/files/hashcat-6.1.1.7z"
+    hashcat_file = "hashcat-6.1.1.7z"
+
     btexec('mkdir dict')
     #check for file existence and download
     print("Installing configparser and other dependencies - needs 'pip3' on path")
@@ -32,20 +41,24 @@ def main():
     print("Checking for dictionary files - will download some if not present...")
     if not is_non_zero_file('dict/breachcompilation.txt'):
         if not is_non_zero_file('empdict.zip'):
-            urllib.request.urlretrieve ("http://www.blacktraffic.co.uk/pw-dict-public/empdict.zip", "empdict.zip")
+            try:
+                urllib.request.urlretrieve (empdict, "empdict.zip")
+            # add error handling so script doesn't break if URL not found    
+            except urllib.error.HTTPError:
+                print('Check the dictionary URLs has not been removed - HTTP error at f"{empdict}" ')
         print("Got dictionary zip, expanding...")    
         zip_ref = zipfile.ZipFile('empdict.zip', 'r')
         zip_ref.extractall('.')
         zip_ref.close()
 
-    if not is_non_zero_file('hashcat-5.1.0.7z'):
+    if not is_non_zero_file(hashcat_file):
         print("Got hashcat-5.1.0, expanding...")    
-        urllib.request.urlretrieve("https://hashcat.net/files/hashcat-5.1.0.7z","hashcat-5.1.0.7z")
-        btexec('7z x hashcat-5.1.0.7z')
+        urllib.request.urlretrieve(hashcat_latest, hashcat_file)
+        btexec('7z x f"{hashcat_file}"')
 
     print("Getting JksPrivkPrepare.jar - for Java keystores")
     if not is_non_zero_file('JksPrivkPrepare.jar'):
-        urllib.request.urlretrieve("https://github.com/floyd-fuh/JKS-private-key-cracker-hashcat/raw/master/JksPrivkPrepare.jar","JksPrivkPrepare.jar")
+        urllib.request.urlretrieve(JksPrivkPrepare,"JksPrivkPrepare.jar")
 
     print("Getting impacket-0.9.19 - might need to get a different one to match the pip install of impacket")
     if not is_non_zero_file('impacket_0_9_19.zip'):
