37cea26658d45525e61e10b67461e0e7a9af03a5
bobbi-hbrown <rholdingbrown@gmail.com>
2022-01-04 17:56:49 +0000

    refactoring some functions

--- setup-with-dict.py
+++ setup-with-dict.py
@@ -23,12 +23,35 @@ def btexec( sexec ):
     
 def main():
 
+    # TODO - convert these to environment variables
     # define dictionary URLs here
-    empdict = "http://www.blacktraffic.co.uk/pw-dict-public/empdict.zip"
-    JksPrivkPrepare = "https://github.com/floyd-fuh/JKS-private-key-cracker-hashcat/raw/master/JksPrivkPrepare.jar"
+    empdict_filename = "empdict.zip"
+    empdict_url = "http://www.blacktraffic.co.uk/pw-dict-public/empdict.zip"
+
+    JksPrivkPrepare_filename = "JksPrivkPrepare.jar"
+    JksPrivkPrepare_url = "https://github.com/floyd-fuh/JKS-private-key-cracker-hashcat/raw/master/JksPrivkPrepare.jar"
+
+    impacket_filename = "impacket_0_9_19.zip"
+    impacket_url = "https://github.com/CoreSecurity/impacket/archive/impacket_0_9_19.zip"
+
+    bleeding_jumbo_filename = "bleeding-jumbo.zip"
+    bleeding_jumbo_url = 
     # change this to your preferred hashcat version
-    hashcat_latest = "https://hashcat.net/files/hashcat-6.1.1.7z"
-    hashcat_file = "hashcat-6.1.1.7z"
+    hashcat_url = "https://hashcat.net/files/hashcat-6.1.1.7z"
+    hashcat_filename = "hashcat-6.1.1.7z"
+
+    def download_files(filename, url, output_message):
+        print(output_message) 
+        if not is_non_zero_file(filename):
+            try:   
+                urllib.request.urlretrieve(url, filename)
+            except urllib.error.HTTPError:
+                print('Check you have supplied the correct URLs/filename versions on lines 26-31: f"{url}" ')  
+    
+    def extract_zip(filename):
+        zip_ref = zipfile.ZipFile(filename, 'r')
+        zip_ref.extractall('.')
+        zip_ref.close()
 
     btexec('mkdir dict')
     #check for file existence and download
@@ -37,48 +60,30 @@ def main():
 
     print("Installing impacket - needs 'pip2' on path. this will only affect Windows IFM formats if it's not installed.")
     btexec("pip2 install impacket==0.9.19")
-       
-    print("Checking for dictionary files - will download some if not present...")
-    if not is_non_zero_file('dict/breachcompilation.txt'):
-        if not is_non_zero_file('empdict.zip'):
-            try:
-                urllib.request.urlretrieve (empdict, "empdict.zip")
-            # add error handling so script doesn't break if URL not found    
-            except urllib.error.HTTPError:
-                print('Check the dictionary URLs has not been removed - HTTP error at f"{empdict}" ')
-        print("Got dictionary zip, expanding...")    
-        zip_ref = zipfile.ZipFile('empdict.zip', 'r')
-        zip_ref.extractall('.')
-        zip_ref.close()
 
-    if not is_non_zero_file(hashcat_file):
-        print("Got hashcat-5.1.0, expanding...")    
-        urllib.request.urlretrieve(hashcat_latest, hashcat_file)
-        btexec('7z x f"{hashcat_file}"')
+    # Download and unzip the dictionaries used for hashing
+    download_files(empdict_filename, empdict_url, "Checking for dictionary files - will download some if not present...")
+    extract_zip(empdict_filename)
 
-    print("Getting JksPrivkPrepare.jar - for Java keystores")
-    if not is_non_zero_file('JksPrivkPrepare.jar'):
-        urllib.request.urlretrieve(JksPrivkPrepare,"JksPrivkPrepare.jar")
+    # Download and unzip your specified version of hashcat
+    download_files(hashcat_filename, hashcat_url, 'Got f"{hashcat_filename}", expanding...')
+    btexec('7z x f"{hashcat_file}"')
 
-    print("Getting impacket-0.9.19 - might need to get a different one to match the pip install of impacket")
-    if not is_non_zero_file('impacket_0_9_19.zip'):
-        urllib.request.urlretrieve("https://github.com/CoreSecurity/impacket/archive/impacket_0_9_19.zip","impacket_0_9_19.zip")
-        
-    zip_ref = zipfile.ZipFile('impacket_0_9_19.zip', 'r')
-    zip_ref.extractall('.')
-    zip_ref.close()
+    # Download JksPrivkPrepare.jar file
+    download_files(JksPrivkPrepare_filename, JksPrivkPrepare_url, "Getting JksPrivkPrepare.jar - for Java keystores")
+
+    # Download and unzip your specified version of impacket, then rename
+    download_files(impacket_filename, impacket_url, "Getting impacket-0.9.19 - might need to get a different one to match the pip install of impacket")
+    extract_zip(impacket_filename)
 
     try:
-        os.rename('impacket-impacket_0_9_19','impacket')
+        os.rename(impacket_filename,'impacket')
     except:
         print("Couldn't rename impacket - assuming already exists")
 
-    if not is_non_zero_file('bleeding-jumbo.zip'):
-        urllib.request.urlretrieve("https://github.com/magnumripper/JohnTheRipper/archive/bleeding-jumbo.zip","bleeding-jumbo.zip")
-        
-    zip_ref = zipfile.ZipFile('bleeding-jumbo.zip', 'r')
-    zip_ref.extractall('.')
-    zip_ref.close()
+    download_files(bleeding_jumbo_filename, bleeding_jumbo_url, 'Got f"{bleeding_jumbo_filename}", expanding...' )
+    extract_zip(bleeding_jumbo_filename)
+
 
     try:        
         os.rename('JohnTheRipper-bleeding-jumbo','john')
